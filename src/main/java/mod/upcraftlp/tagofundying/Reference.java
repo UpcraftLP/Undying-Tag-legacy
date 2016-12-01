5d147409386b6bac1f0686189b3c6ec309fddd26
Unknown <j-schirra@gmx.de>
2016-12-01 13:20:34 +0100

    released v1.0.1

--- src/main/java/mod/upcraftlp/tagofundying/Reference.java
+++ src/main/java/mod/upcraftlp/tagofundying/Reference.java
@@ -3,7 +3,7 @@ package mod.upcraftlp.tagofundying;
 public class Reference {
 
 	public static final String MCVERSION = "1.11";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	
 	/** TEAM **/
 	public static final String[] authors = {"UpcraftLP"};
